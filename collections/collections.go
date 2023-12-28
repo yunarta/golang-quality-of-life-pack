c35d5a4ed861a5715c644b59acb634b475d0dc18
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2023-12-29 02:45:16 +0800

    Add comprehensive tests and update functions in collections package
    This commit introduces comprehensive tests for functions in the collections package and performs minor updates to some functions for better clarity. The functions updated include Delta, Contains, EqualsIgnoreOrder, Unique, Intersect, Map, MapNotNull, GetValuesOfMap, and SortStrings. The tests added cover a wide variety of cases to ensure the robustness of the functions.

--- collections/collections.go
+++ collections/collections.go
@@ -2,66 +2,83 @@ package collections
 
 import "sort"
 
-func Delta[T comparable](target []T, source []T) (adding []T, removing []T) {
+// Delta function is used to calculate the difference between two slices.
+// It compares 'source' and 'target' slices and determines which elements are to be added or removed from the 'source'
+// slice to match the 'target' slice.
+func Delta[T comparable](source []T, target []T) (adding []T, removing []T) {
+
+	// Prepare slices to store elements that need to be added or removed
 	var (
 		elementsToBeAdded   = make([]T, 0)
 		elementsToBeRemoved = make([]T, 0)
 	)
 
-	for _, missingItem := range source {
-		if !Contains(target, missingItem) {
+	// Loop over 'source' slice to identify items missing in the 'target'. These will be added.
+	for _, missingItem := range target {
+		// If a 'source item' doesn't exist in 'target', it needs to be added
+		if !Contains(source, missingItem) {
 			elementsToBeAdded = append(elementsToBeAdded, missingItem)
 		}
 	}
 
-	for _, extraItem := range target {
-		if !Contains(source, extraItem) {
+	// Similar to previous loop, iterate over 'target' slice to determine which items are not present in 'source'. These will be removed.
+	for _, extraItem := range source {
+		// If a 'target item' doesn't exist in 'source', it needs to be removed
+		if !Contains(target, extraItem) {
 			elementsToBeRemoved = append(elementsToBeRemoved, extraItem)
 		}
 	}
 
+	// Return both slice of elements that need to be added and removed to sync 'target' with 'source'
 	return elementsToBeAdded, elementsToBeRemoved
 }
 
+// Contains is a helper function that checks whether a specific element ('check') exists in the provided slice ('source')
 func Contains[T comparable](source []T, check T) bool {
+	// Loop over each item in the slice
 	for _, v := range source {
+		// If the current item matches the item we're checking for, return 'true'
 		if v == check {
 			return true
 		}
 	}
+	// If we've looped over all the items and haven't found a match, return 'false'
 	return false
 }
 
+// EqualsIgnoreOrder compares two slices and returns 'true' if they contain the same elements, regardless of their order
 func EqualsIgnoreOrder[T comparable](source, other []T) bool {
+	// If the lengths of both slices aren't equal, they can't be the same
 	if len(source) != len(other) {
 		return false
 	}
 
+	// Otherwise, start by counting the occurrences of each element in 'source'
 	counts := make(map[T]int)
 	for _, item := range source {
 		counts[item]++
 	}
 
+	// Then, reduce the count for each 'other' item in the map
 	for _, item := range other {
+		// If an element doesn't exist in 'source', the slices aren't the same
 		if counts[item] == 0 {
 			return false
 		}
 		counts[item]--
 	}
 
-	for _, count := range counts {
-		if count != 0 {
-			return false
-		}
-	}
-
+	// If all counts are zero, the slices are equal, return 'true'
 	return true
 }
 
+// Unique function goes through a slice and eliminates the duplicate elements
 func Unique[T comparable](collection []T) []T {
+	// Prepare a map to hold unique elements
 	uniqueMap := make(map[T]bool)
-	var uniqueSlice = make([]T, 0)
+	uniqueSlice := make([]T, 0)
 
+	// Iterate over the collection and add each unique item to both our map and our result slice
 	for _, value := range collection {
 		if _, exists := uniqueMap[value]; !exists {
 			uniqueMap[value] = true
@@ -69,38 +86,54 @@ func Unique[T comparable](collection []T) []T {
 		}
 	}
 
+	// Return slice of unique elements
 	return uniqueSlice
 }
 
+// Intersect function returns a slice containing common elements between 'source' and 'other'.
 func Intersect[T comparable](source, other []T) []T {
+	// Map to keep track of elements in 'source'
 	elemMap := make(map[T]bool)
-	var result = make([]T, 0)
+	// Resulting slice to store common elements
+	result := make([]T, 0)
 
+	// Loop over 'source' and add each element to the map
 	for _, v := range source {
 		elemMap[v] = true
 	}
 
+	// Loop over 'other'. If an element also exists in our map, add it to the result
 	for _, v := range other {
 		if _, ok := elemMap[v]; ok {
 			result = append(result, v)
+			// Avoid duplicates in the result by deleting the element from the map
 			delete(elemMap, v)
 		}
 	}
 
+	// Return the slice of common elements
 	return result
 }
 
+// Map function applies a provided function 'mapper' on each element of a given slice.
+// It's a generalized function to perform any operation 'mapper' on all elements
 func Map[K any](collection []K, mapper func(k K) K) []K {
-	var slice []K
+	slice := make([]K, 0)
+
+	// For each element in 'collection', apply the 'mapper' function and store the result in 'slice'
 	for _, item := range collection {
 		slice = append(slice, mapper(item))
 	}
 
+	// Return the slice containing results of 'mapper' function applied on each item of 'collection'
 	return slice
 }
 
+// MapNotNull function applies a function 'mapper' on each element of a slice, but only considers non-nil results.
 func MapNotNull[K any](collection []K, mapper func(k K) *K) []K {
-	var slice []K
+	slice := make([]K, 0)
+
+	// Loop over 'collection'. If 'mapper' returns a non-nil value, add it to 'slice'
 	for _, item := range collection {
 		k := mapper(item)
 		if k != nil {
@@ -108,20 +141,26 @@ func MapNotNull[K any](collection []K, mapper func(k K) *K) []K {
 		}
 	}
 
+	// Return the slice containing non-nil results
 	return slice
 }
 
-func MapValues[K comparable, V any](source map[K]V) []V {
-	var values []V
+// GetValuesOfMap function fetches all values from the supplied map and returns them as a slice.
+func GetValuesOfMap[K comparable, V any](source map[K]V) []V {
+	values := make([]V, 0)
 
+	// Loop over the source map and collect its values
 	for _, value := range source {
 		values = append(values, value)
 	}
 
+	// Return the slice of values
 	return values
 }
 
+// SortStrings function sorts a slice of strings in alphabetical order.
 func SortStrings(source []string) []string {
 	sort.Strings(source)
+	// Return sorted slice
 	return source
 }
---
+++ collections/collections_test.go
@@ -0,0 +1,367 @@
+package collections
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDelta(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{1, 2, 3}
+		expectedAdds := []int{}
+		expectedRemoves := []int{4, 5}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	// Add test cases here
+	t.Run("test with empty slices", func(t *testing.T) {
+		emptySlice := []int{}
+		expectedAdds := []int{1, 2, 3}
+		expectedRemoves := []int{}
+		actualAdds, actualRemoves := Delta(emptySlice, []int{1, 2, 3})
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with strings", func(t *testing.T) {
+		a := []string{"apple", "banana", "orange"}
+		b := []string{"banana", "orange", "grape"}
+		expectedAdds := []string{"grape"}
+		expectedRemoves := []string{"apple"}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with empty string slices", func(t *testing.T) {
+		emptySlice := []string{}
+		expectedAdds := []string{"apple", "banana", "cherry"}
+		expectedRemoves := []string{}
+		actualAdds, actualRemoves := Delta(emptySlice, []string{"apple", "banana", "cherry"})
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with string slice and empty slice", func(t *testing.T) {
+		emptySlice := []string{}
+		expectedAdds := []string{}
+		expectedRemoves := []string{"apple", "banana", "cherry"}
+		actualAdds, actualRemoves := Delta([]string{"apple", "banana", "cherry"}, emptySlice)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with duplicate elements", func(t *testing.T) {
+		// Test case with duplicate elements in both slices
+		a := []int{1, 2, 2, 3, 4, 5}
+		b := []int{2, 3, 3, 4, 5, 5}
+		expectedAdds := []int{}
+		expectedRemoves := []int{1}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	// New test cases
+	t.Run("test with nil slices", func(t *testing.T) {
+		var a, b []int
+		expectedAdds, expectedRemoves := []int{}, []int{}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with nil and non-nil slices", func(t *testing.T) {
+		var a []int
+		b := []int{1, 2, 3, 4, 5}
+		expectedAdds := []int{1, 2, 3, 4, 5}
+		expectedRemoves := []int{}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with completely different integer slices", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{-1, -2, -3, -4, -5}
+		expectedAdds := []int{-1, -2, -3, -4, -5}
+		expectedRemoves := []int{1, 2, 3, 4, 5}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with partially different string slices", func(t *testing.T) {
+		a := []string{"a", "b", "c", "d", "e"}
+		b := []string{"d", "e", "f", "g", "h"}
+		expectedAdds := []string{"f", "g", "h"}
+		expectedRemoves := []string{"a", "b", "c"}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with same string slices", func(t *testing.T) {
+		a := []string{"alea", "iacta", "est"}
+		b := []string{"alea", "iacta", "est"}
+		expectedAdds := []string{}
+		expectedRemoves := []string{}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with different order of elements in slices", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{5, 4, 3, 2, 1}
+		expectedAdds := []int{}
+		expectedRemoves := []int{}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+
+	t.Run("test with one empty string in one of the slices", func(t *testing.T) {
+		a := []string{"alea", "iacta", ""}
+		b := []string{"alea", "iacta", "est"}
+		expectedAdds := []string{"est"}
+		expectedRemoves := []string{""}
+		actualAdds, actualRemoves := Delta(a, b)
+		assert.ElementsMatch(t, expectedAdds, actualAdds)
+		assert.ElementsMatch(t, expectedRemoves, actualRemoves)
+	})
+}
+
+func TestContains(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+
+		assert.True(t, Contains(a, 1))
+		assert.False(t, Contains(a, 10))
+
+	})
+
+	t.Run("test with empty slice", func(t *testing.T) {
+		emptySlice := []int{}
+		assert.False(t, Contains(emptySlice, 1))
+	})
+
+}
+
+func TestEqualsIgnoreOrder(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{5, 4, 3, 2, 1}
+
+		assert.True(t, EqualsIgnoreOrder(a, b))
+	})
+
+	t.Run("test with empty slices", func(t *testing.T) {
+		emptySlice := []int{}
+		assert.True(t, EqualsIgnoreOrder(emptySlice, emptySlice))
+	})
+
+	t.Run("test with different integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{6, 7, 8, 9, 10}
+
+		assert.False(t, EqualsIgnoreOrder(a, b))
+	})
+
+	t.Run("test with different number of integers", func(t *testing.T) {
+		a := []int{1, 2, 3}
+		b := []int{1, 2, 3, 4}
+
+		assert.False(t, EqualsIgnoreOrder(a, b))
+	})
+
+	t.Run("test with same integers but with duplicates in both slice", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{1, 2, 2, 3, 4, 5}
+
+		assert.False(t, EqualsIgnoreOrder(a, b))
+	})
+
+	t.Run("test with same number of integers but different content", func(t *testing.T) {
+		a := []int{11, 11, 11, 12}
+		b := []int{11, 11, 12, 12}
+
+		assert.False(t, EqualsIgnoreOrder(a, b))
+	})
+}
+
+func TestUnique(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 2, 3, 3, 3}
+		expected := []int{1, 2, 3}
+
+		assert.ElementsMatch(t, expected, Unique(a))
+	})
+
+	t.Run("test with duplicate strings", func(t *testing.T) {
+		// Test case with duplicate strings
+		a := []string{"apple", "banana", "cherry", "banana", "apple"}
+		expected := []string{"apple", "banana", "cherry"}
+		actual := Unique(a)
+
+		assert.ElementsMatch(t, actual, expected)
+	})
+}
+
+func TestIntersect(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{1, 2, 3}
+		expected := []int{1, 2, 3}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with same list of integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{1, 2, 3, 4, 5}
+		expected := []int{1, 2, 3, 4, 5}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with no intersect integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		b := []int{6, 7, 8, 9, 10}
+		expected := []int{}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with empty slices", func(t *testing.T) {
+		a := []int{}
+		b := []int{1, 2, 3, 4, 5}
+		expected := []int{}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with strings", func(t *testing.T) {
+		a := []string{"apple", "banana", "cherry"}
+		b := []string{"banana", "cherry", "grape"}
+		expected := []string{"banana", "cherry"}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with same list of strings", func(t *testing.T) {
+		a := []string{"apple", "banana", "cherry"}
+		b := []string{"apple", "banana", "cherry"}
+		expected := []string{"apple", "banana", "cherry"}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with no intersect strings", func(t *testing.T) {
+		a := []string{"apple", "banana", "cherry"}
+		b := []string{"grape", "melon", "lemon"}
+		expected := []string{}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with a string slice and an empty string slice", func(t *testing.T) {
+		a := []string{"apple", "banana", "cherry"}
+		b := []string{}
+		expected := []string{}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with a string slice and a nil string slice", func(t *testing.T) {
+		a := []string{"apple", "banana", "cherry"}
+		var b []string
+		expected := []string{}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+
+	t.Run("test with two empty slices", func(t *testing.T) {
+		a := []int{}
+		b := []int{}
+		expected := []int{}
+
+		assert.ElementsMatch(t, expected, Intersect(a, b))
+	})
+}
+
+func TestMap(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 3}
+		expected := []int{2, 4, 6}
+		actual := Map(a, func(k int) int { return k * 2 })
+
+		assert.ElementsMatch(t, actual, expected)
+	})
+}
+
+func TestMapNotNull(t *testing.T) {
+	t.Run("test with integers", func(t *testing.T) {
+		a := []int{1, 2, 3, 4, 5}
+		expected := []int{1, 2, 3}
+		actual := MapNotNull(a, func(k int) *int {
+			if k > 3 {
+				return nil
+			}
+			return &k
+		})
+
+		assert.ElementsMatch(t, actual, expected)
+	})
+}
+
+func TestGetValuesOfMap(t *testing.T) {
+	t.Run("test with string to int map", func(t *testing.T) {
+		a := map[string]int{"one": 1, "two": 2, "three": 3}
+		expected := []int{1, 2, 3}
+		actual := GetValuesOfMap(a)
+
+		assert.ElementsMatch(t, actual, expected)
+	})
+}
+
+func TestSortStrings(t *testing.T) {
+	t.Run("test with unsorted strings", func(t *testing.T) {
+		unsortedStrSlice := []string{"banana", "apple", "cherry"}
+		expected := []string{"apple", "banana", "cherry"}
+
+		actual := SortStrings(unsortedStrSlice)
+
+		assert.ElementsMatch(t, expected, actual)
+	})
+
+	t.Run("test with sorted strings", func(t *testing.T) {
+		sortedStrSlice := []string{"apple", "banana", "cherry"}
+		expected := []string{"apple", "banana", "cherry"}
+
+		actual := SortStrings(sortedStrSlice)
+
+		assert.ElementsMatch(t, expected, actual)
+	})
+
+	t.Run("test with empty string slice", func(t *testing.T) {
+		emptyStrSlice := []string{}
+		expected := []string{}
+
+		actual := SortStrings(emptyStrSlice)
+
+		assert.ElementsMatch(t, expected, actual)
+	})
+
+	t.Run("test with one element string slice", func(t *testing.T) {
+		oneElementStrSlice := []string{"apple"}
+		expected := []string{"apple"}
+
+		actual := SortStrings(oneElementStrSlice)
+
+		assert.ElementsMatch(t, expected, actual)
+	})
+}
